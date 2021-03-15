f831284a3f4e94e4b3c58a0bbb39c606b8f1930e
vlajko32 <vlajko247@gmail.com>
2021-03-15 21:20:30 +0100

    Napravljena klasa MagacinImplementacija

---
+++ src/logika/MagacinImplementacija.java
@@ -0,0 +1,25 @@
+package logika;
+
+import domen.Artikal;
+
+public class MagacinImplementacija implements MagazinInterface{
+
+	@Override
+	public void dodajArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pronadjiArtikal(int sifra) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
