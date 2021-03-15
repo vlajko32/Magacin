0ff914e5039e40303f4023e93e8ed70ba66004a2
vlajko32 <vlajko247@gmail.com>
2021-03-15 21:38:05 +0100

    Implementirane metode u klasi MagacinImplementacija

--- src/logika/MagacinImplementacija.java
+++ src/logika/MagacinImplementacija.java
@@ -1,24 +1,59 @@
 package logika;
 
+import java.util.LinkedList;
+
 import domen.Artikal;
 
 public class MagacinImplementacija implements MagazinInterface{
 
+	private LinkedList<Artikal> artikli = new LinkedList<Artikal>();
+
 	@Override
 	public void dodajArtikal(Artikal artikal) {
-		// TODO Auto-generated method stub
+		if(artikal == null)
+			throw new NullPointerException("Artikal ne sme da bude null");
+		for (Artikal a : artikli) {
+			if(a.getSifra() == artikal.getSifra()) {
+				a.setKolicina(a.getKolicina() + 1);
+				return;
+			}
+		}
 		
+		artikli.add(artikal);
 	}
 
 	@Override
 	public void izbaciArtikal(Artikal artikal) {
-		// TODO Auto-generated method stub
+		
+		if(artikal == null)
+			throw new NullPointerException("Artikal je null");
+		
+		if(!artikli.contains(artikal))
+			throw new RuntimeException("Artikal ne postoji u trenutnoj listi artikala");
+
+		
+		artikli.remove(artikal);
 		
 	}
 
 	@Override
-	public void pronadjiArtikal(int sifra) {
-		// TODO Auto-generated method stub
+	public Artikal pronadjiArtikal(int sifra) {
+		
+		Artikal rezultat = null;
+		
+		if(sifra <= 0)
+			throw new RuntimeException("Neispravna vrednost!");
+		
+		for (int i = 0;i<artikli.size();i++) {
+			if(artikli.get(i).getSifra() == sifra)
+				rezultat = artikli.get(i);
+		}
+		
+		if(rezultat == null)
+			throw new RuntimeException("Artikal sa datom sifrom nije pronadjen");
+		else
+			return rezultat;
+
 		
 	}
 
--- src/logika/MagazinInterface.java
+++ src/logika/MagazinInterface.java
@@ -5,5 +5,5 @@ import domen.Artikal;
 public interface MagazinInterface {
 	public void dodajArtikal(Artikal artikal);
 	public void izbaciArtikal(Artikal artikal);
-	public void pronadjiArtikal(int sifra);
+	public Artikal pronadjiArtikal(int sifra);
 }
